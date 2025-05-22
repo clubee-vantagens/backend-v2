5157ca25f3f3e1e4a2dd44db9ce5ee4abee976fa
Giovanni Marques <giovanni.marques030@gmail.com>
2025-05-22 15:57:55 -0300

    chore: configuração inicial do projeto

---
+++ clubee/src/test/java/com/clubee/ClubeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clubee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
